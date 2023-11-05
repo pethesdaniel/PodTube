74d07d94f02eb5af84ccda6f24294283f107cb72
Daniel Imre Pethes <16624998+pethesdaniel@users.noreply.github.com>
2023-11-05 01:52:55 +0100

    Refactor + Video Player

--- PodTube/Client/Editor/Util/ServiceCollectionExtensions.cs
+++ PodTube/Client/Editor/Util/ServiceCollectionExtensions.cs
@@ -1,10 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using PodTube.Client.Editor.Services;
-using Radzen;
+﻿using Radzen;
 namespace PodTube.Client.Editor.Util {
     public static class ServiceCollectionExtensions {
         public static void AddEditorServices(this IServiceCollection services) {
-            services.AddSingleton<TimelineService>();
             services.AddRadzenComponents();
         }
     }

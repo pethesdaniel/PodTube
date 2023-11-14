03c751938333f790324dd8a1ba7791f6fc669620
Daniel Imre Pethes <16624998+pethesdaniel@users.noreply.github.com>
2023-11-14 03:08:09 +0100

    Early implementation of multitrack playback

---
+++ PodTube/Client/Components/Videos/VideoPlayer/Audio.cs
@@ -0,0 +1,6 @@
+﻿namespace PodTube.Client.Components.Videos.VideoPlayer {
+    public class Audio {
+        public Guid Guid { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ PodTube/Client/Components/Videos/VideoPlayer/AudioController.cs
@@ -0,0 +1,36 @@
+﻿namespace PodTube.Client.Components.Videos.VideoPlayer {
+    public class AudioController {
+        public event Action<string> PlayAudioWithUrlEvent;
+        public event Action<string> PauseAudioWithUrlEvent;
+        public event Action<string, double> SeekAudioWithUrlEvent;
+        public event Action<double> SetVolumeEvent;
+
+        public event Action<AudioMetadata> OnMetadataLoadedEvent;
+        public event Action<AudioPlayerState> OnTimeUpdatedEvent;
+
+
+        public void PlayAudioWithUrl(string url) {
+            PlayAudioWithUrlEvent?.Invoke(url);
+        }
+
+        public void PauseAudioWithUrl(string url) {
+            PauseAudioWithUrlEvent?.Invoke(url);
+        }
+
+        public void SeekAudioWithUrl(string url, double time) {
+            SeekAudioWithUrlEvent?.Invoke(url, time);
+        }
+
+        public void SetVolume(double volume) {
+            SetVolumeEvent?.Invoke(volume);
+        }
+
+        public void OnTimeUpdated(AudioPlayerState metadata) {
+            OnTimeUpdatedEvent?.Invoke(metadata);
+        }
+
+        public void OnMetadataLoaded(AudioMetadata metadata) {
+            OnMetadataLoadedEvent?.Invoke(metadata);
+        }
+    }
+}
